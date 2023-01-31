99d5886633fbc198f8887ad093a8326ee8a0627f
Marko-Kushta <markokushta39@gmail.com>
2023-01-31 01:21:37 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Main</title>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body>
+    <header class = "nav">
+        <div class = "nav-margin">
+            <nav class = "navigation">
+            <a href = "./index.php"><figure class = "header-logo">WoW</figure></a>
+                <ul class = "links">
+                    <li>
+                        <a href = "./login.php" target = "_blank">Login</a>
+                    </li>
+                    <li>
+                        <a href = "./register.php" target = "_blank">Register</a>
+                    </li>
+                    <li>
+                        <a href = "./armory.php" target = "_blank">Armory</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div class = "nav-margin">
+            <ul class = "links">
+                a
+    </header>
+	</body>
+</html>
